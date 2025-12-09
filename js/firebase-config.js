e4ec0509e99a72568d9139d6dc2f4cd3743401d3
SowmisHub <vemulasowmika123@gmail.com>
2025-12-09 15:44:44 +0530

    time tracking web

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBFlx7tNISTTOatX6_QWTbu4p4ztjn-cEs",
     authDomain: "time-tracking-web-application.firebaseapp.com",
     projectId: "time-tracking-web-application",
-    storageBucket: "time-tracking-web-application.firebasestorage.app",
+    storageBucket: "time-tracking-web-application.appspot.com",
     messagingSenderId: "516236781054",
     appId: "1:516236781054:web:873c9a4577e6b4deac0caf"
 };
